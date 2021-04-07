28fb418bce61570e69a1ca3aebf6a90dc4e89f37
Juan Cruz Tourret <jctourret@gmail.com>
2021-04-07 06:18:03 -0300

    Began project, generated galaxy, added trails and ship.
    Nota a futuro: releer la definición de "versionado atomico".

---
+++ Solar System/Assets/Scripts/lightChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightChange : MonoBehaviour
+{
+    Light sunlight;
+    float fluctuation = 0;
+    float baseIntensity = 10;
+    float multiplier = 5;
+    void Start()
+    {
+        sunlight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        fluctuation += Time.deltaTime;
+        sunlight.intensity = Mathf.Sin(fluctuation)*multiplier+baseIntensity;
+    }
+}
